82575c2918c919d0cfdb2fa3259a06a26788c10e
guillermocruzgarri <gcgarri@gmail.com>
2025-10-22 11:46:26 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -30,10 +30,11 @@ const Header = () => {
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
+          {/*
           <div className="font-bold text-xl text-blue-600">
             Guillermo Cruz
           </div>
-
+          */}
           {/* Desktop Navigation */}
           <nav className="hidden md:flex space-x-8">
             {['Home', 'About', 'Skills', 'Experience', 'Portfolio', 'Blog', 'Contact'].map((item) => (
